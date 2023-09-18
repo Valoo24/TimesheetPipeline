f19716243c290b3ca584e3af7be9170f7c2c47a2
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-09-18 10:15:07 +0200

    Création d'un IRepository dans le domain et d'un HolidayRepository dans la Persistence. Ajout des références de projets nécessaires. Ajout d'injection de dépendance du Repository. Test Ok.

--- TimesheetPipeline/Timesheet.API/Program.cs
+++ TimesheetPipeline/Timesheet.API/Program.cs
@@ -1,4 +1,5 @@
 using Timesheet.Application.Services;
+using Timesheet.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<HolidayService, HolidayService>();
+builder.Services.AddScoped<HolidayRepository, HolidayRepository>();
 
 var app = builder.Build();
 
