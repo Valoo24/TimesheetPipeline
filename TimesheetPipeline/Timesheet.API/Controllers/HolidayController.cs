77df37697c922e0be11d24de4fe28362c9dccac1
PCBENJAMIN\benja <benjamin_malempree@hotmail.com>
2023-09-18 09:19:03 +0200

    Création d'un HolidayController avec une méthode GetAll. Test Ok.

---
+++ TimesheetPipeline/Timesheet.API/Controllers/HolidayController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Timesheet.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class HolidayController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Accès à la méthode pour avoir toutes les Vacances.");
+        }
+    }
+}
