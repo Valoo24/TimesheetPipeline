47014ecd6aa13d5364c25930d37cc4e0a5c8907c
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-09-18 10:30:36 +0200

    Ajout des fichiers CSV dans l'application pour la persistence des données. Test Ok.

--- TimesheetPipeline/Timesheet.Persistence/Repositories/HolidayRepository.cs
+++ TimesheetPipeline/Timesheet.Persistence/Repositories/HolidayRepository.cs
@@ -10,67 +10,51 @@ namespace Timesheet.Persistence.Repositories
 {
     public class HolidayRepository : IRepository<Holiday, int>
     {
+        private string _csvFilePath
+        {
+            get
+            {
+                string AppPath = AppDomain.CurrentDomain.BaseDirectory;
+                return Path.Combine(AppPath, "HolidayDataBase.csv");
+            }
+        }
+
         public IEnumerable<Holiday> GetAll()
         {
-            return new List<Holiday>{
-                new Holiday
+            IList<Holiday> holidayList = new List<Holiday>();
+
+            using (StreamReader reader = new StreamReader(_csvFilePath))
+            {
+                reader.ReadLine();
+
+                while (!reader.EndOfStream)
                 {
-                    Id = 1,
-                    Name = "Nouvel an",
-                    Date = new DateTime(2024, 1, 1)
-                },
-                new Holiday
-                {
-                    Id = 2,
-                    Name = "Lundi de Pâques",
-                },
-                new Holiday
-                {
-                    Id = 3,
-                    Name = "Fête du travail",
-                    Date = new DateTime(2023, 5, 1)
-                },
-                new Holiday
-                {
-                    Id = 4,
-                    Name = "Ascension"
-                },
-                new Holiday
-                {
-                    Id = 5,
-                    Name = "Lundi de Pentecôte"
-                },
-                new Holiday
-                {
-                    Id = 6,
-                    Name = "Fête nationale de Belgique",
-                    Date = new DateTime(2023, 7, 21)
-                },
-                new Holiday
-                {
-                    Id = 7,
-                    Name = "Assomption",
-                    Date = new DateTime(2023, 8, 15)
-                },
-                new Holiday
-                {
-                    Id = 8,
-                    Name = "Toussaint",
-                    Date = new DateTime(2023, 11, 1)
-                },
-                new Holiday
-                {
-                    Id = 9,
-                    Name = "Armistice",
-                    Date = new DateTime(2023, 11, 11)
-                },
-                new Holiday
-                {
-                    Id = 10,
-                    Name = "Noël",
-                    Date = new DateTime(2023, 12, 25)
+                    string line = reader.ReadLine();
+                    string[] values = line.Split(';');
+
+                    string idString = values[0].Trim();
+                    string name = values[1].Trim();
+                    string dateTimeString = values[2].Trim();
+
+                    int HolidayId;
+                    int.TryParse(idString, out HolidayId);
+
+                    string HolidayName = name;
+
+                    DateTime HolidayDate; 
+                    DateTime.TryParse(dateTimeString, out HolidayDate);
+
+                    Holiday holiday = new Holiday
+                    { 
+                        Id = HolidayId,
+                        Name = HolidayName,
+                        Date = HolidayDate
+                    };
+
+                    holidayList.Add(holiday);
                 }
-            };
+            }
+            return holidayList;
         }
     }
 }
