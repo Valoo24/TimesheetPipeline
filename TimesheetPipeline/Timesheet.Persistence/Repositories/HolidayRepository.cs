1c5f93009aac4c38fad027e3f86c5ba870c52374
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-09-18 11:03:07 +0200

    Création d'une méthode dans le HolidayRepository afin d'initialiser le fichier HolidayDataBase.csv.

--- TimesheetPipeline/Timesheet.Persistence/Repositories/HolidayRepository.cs
+++ TimesheetPipeline/Timesheet.Persistence/Repositories/HolidayRepository.cs
@@ -41,11 +41,11 @@ namespace Timesheet.Persistence.Repositories
 
                     string HolidayName = name;
 
-                    DateTime HolidayDate; 
+                    DateTime HolidayDate;
                     DateTime.TryParse(dateTimeString, out HolidayDate);
 
                     Holiday holiday = new Holiday
-                    { 
+                    {
                         Id = HolidayId,
                         Name = HolidayName,
                         Date = HolidayDate
@@ -60,6 +60,79 @@ namespace Timesheet.Persistence.Repositories
         public Holiday GetById(int id)
         {
             return GetAll().FirstOrDefault(h => h.Id == id);
+        }
+
+        public void InitializeHolidayCSV()
+        {
+            IEnumerable<Holiday> HolidayList = new List<Holiday>
+            {
+                new Holiday
+                {
+                    Id = 1,
+                    Name = "Nouvel an",
+                    Date = new DateTime(2024, 1, 1)
+                },
+                new Holiday
+                {
+                    Id = 2,
+                    Name = "Lundi de Pâques",
+                },
+                new Holiday
+                {
+                    Id = 3,
+                    Name = "Fête du travail",
+                    Date = new DateTime(2023, 5, 1)
+                },
+                new Holiday
+                {
+                    Id = 4,
+                    Name = "Ascension"
+                },
+                new Holiday
+                {
+                    Id = 5,
+                    Name = "Lundi de Pentecôte"
+                },
+                new Holiday
+                {
+                    Id = 6,
+                    Name = "Fête nationale de Belgique",
+                    Date = new DateTime(2023, 7, 21)
+                },
+                new Holiday
+                {
+                    Id = 7,
+                    Name = "Assomption",
+                    Date = new DateTime(2023, 8, 15)
+                },
+                new Holiday
+                {
+                    Id = 8,
+                    Name = "Toussaint",
+                    Date = new DateTime(2023, 11, 1)
+                },
+                new Holiday
+                {
+                    Id = 9,
+                    Name = "Armistice",
+                    Date = new DateTime(2023, 11, 11)
+                },
+                new Holiday
+                {
+                    Id = 10,
+                    Name = "Noël",
+                    Date = new DateTime(2023, 12, 25)
+                }
+            };
+
+            using (StreamWriter writer = new StreamWriter(_csvFilePath, false, Encoding.UTF8))
+            {
+                writer.WriteLine("Id,Name,Date");
+                foreach (Holiday holiday in HolidayList)
+                {
+                    writer.WriteLine($"{holiday.Id},{holiday.Name},{holiday.Date}");
+                }
+            }
         }
     }
 }
