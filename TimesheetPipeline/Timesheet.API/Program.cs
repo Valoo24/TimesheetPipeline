eaff4a53250cf96284583f8bd1f37f6a1d296a89
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-09-25 11:10:29 +0200

    Création d'un TimesheetContext pour gérer une base de donnée avec Entity Framework. Création d'options de configuration dans le startup de l'API et dans le fichier Appsettings.json.

--- TimesheetPipeline/Timesheet.API/Program.cs
+++ TimesheetPipeline/Timesheet.API/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Timesheet.Application.Services;
 using Timesheet.Domain.Interfaces;
+using Timesheet.Infrastrucutre.DataAccess;
 using Timesheet.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TimesheetContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("TimesheetConnection"))
+    .EnableSensitiveDataLogging()
+    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
 builder.Services.AddScoped<IHolidayService, HolidayService>();
 builder.Services.AddScoped<IHolidayRepository, HolidayRepository>();
