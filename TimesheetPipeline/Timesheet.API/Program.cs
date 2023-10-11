7150a7c780438272c0577535efea6e288e2da53b
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-10-11 14:01:46 +0200

    Re-modification du program.cs et du launchsettings.json de l'api afin de ne plus avoir de dépendance à l'UI. Retrait de la référence de projet de l'API à l'UI Blazor.

--- TimesheetPipeline/Timesheet.API/Program.cs
+++ TimesheetPipeline/Timesheet.API/Program.cs
@@ -22,39 +22,17 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ITimesheetRepository, TimesheetRepository>();
 builder.Services.AddScoped<ITimesheetService, TimesheetService>();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-});
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
-if(app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
-
-app.UseStaticFiles();
-
 app.UseAuthorization();
 
-app.UseCors("Open");
-
 app.MapControllers();
-
-app.MapFallbackToFile("index.html");
 
 app.Run();
