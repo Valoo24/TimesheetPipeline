0aec7e6b45296e2044d67db02b3395fa5a810d1d
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-10-13 08:52:33 +0200

    Création de l'interface du TokenManager

---
+++ TimesheetPipeline/Timesheet.Domain/Interfaces/ITokenManager.cs
@@ -0,0 +1,13 @@
+﻿using Timesheet.Domain.Entities.Users;
+
+namespace Timesheet.Domain.Interfaces
+{
+    public interface ITokenManager
+    {
+        public string Issuer { get; }
+        public string Audience { get; }
+        public string Secret { get; }
+
+        public string GenerateToken(User user);
+    }
+}
