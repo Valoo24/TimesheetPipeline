c5876b269fc460e1cf2a819284e9ebd44b4dbf6e
Benjamin Malemprée <benjamin_malempree@hotmail.com>
2023-10-18 10:24:50 +0200

    Création d'un middleware GlobalExceptionHandlingMiddelware afin de gérer les exceptions dans l'api.

---
+++ TimesheetPipeline/Timesheet.API/Middelwares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace Timesheet.API.Middelwares
+{
+    public class GlobalExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public GlobalExceptionHandlingMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            _logger = logger;
+
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, ex.Message);
+
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+        }
+    }
+}
